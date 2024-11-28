af8e7edb412b1bf3e86b130b787a9b82f16d24f2
Daniel Gullberg <daniel@gullberg.dev>
2024-11-28 16:10:52 +0100

    init with day template

---
+++ src/advent_of_code/day_template.clj
@@ -0,0 +1,25 @@
+(ns advent-of-code.day-template
+  (:require [ysera.test :refer [is= is is-not]]))
+
+(def input (slurp "src/advent_of_code/year_2024/inputs/day01.txt"))
+(def test-input "")
+
+(defn part-1
+  {:test (fn []
+           (is= (part-1 test-input) 42))}
+  [input]
+  42)
+
+(defn part-2
+  {:test (fn []
+           (is= (part-2 test-input) 42))}
+  [input]
+  42)
+
+(comment
+  ;;
+  (time (part-1 input))
+
+  ;;
+  (time (part-2 input))
+  )
