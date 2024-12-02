0c3760251b7801ea092d2d7aaee34a320bb39dfc
Daniel Gullberg <daniel@gullberg.dev>
2024-12-02 11:00:20 +0100

    day 1

--- src/advent_of_code/year_2024/day_01.clj
+++ src/advent_of_code/year_2024/day_01.clj
@@ -2,24 +2,45 @@
   (:require [ysera.test :refer [is= is is-not]]))
 
 (def input (slurp "src/advent_of_code/year_2024/inputs/day01.txt"))
-(def test-input "")
+(def test-input "3   4\n4   3\n2   5\n1   3\n3   9\n3   3")
 
 (defn part-1
   {:test (fn []
-           (is= (part-1 test-input) 42))}
+           (is= (part-1 test-input) 11))}
   [input]
-  42)
+  (let [[list1 list2] (->> (clojure.string/split-lines input)
+                           (reduce (fn [[a1 a2] line]
+                                     (let [[v1 v2] (->> (re-seq #"\d+" line)
+                                                        (map read-string))]
+                                       [(conj a1 v1) (conj a2 v2)]))
+                                   [[] []])
+                           (map sort))]
+    (->> (map (comp abs -) list1 list2)
+         (reduce +))))
 
 (defn part-2
   {:test (fn []
-           (is= (part-2 test-input) 42))}
+           (is= (part-2 test-input) 31))}
   [input]
-  42)
+  (let [[list1 list2] (->> (clojure.string/split-lines input)
+                           (reduce (fn [[a1 a2] line]
+                                     (let [[v1 v2] (->> (re-seq #"\d+" line)
+                                                        (map read-string))]
+                                       [(conj a1 v1) (conj a2 v2)]))
+                                   [[] []])
+                           (map sort))
+        freqs (frequencies list2)]
+    (reduce (fn [a v]
+              (+ a (* v (get freqs v 0))))
+            0
+            list1)))
 
 (comment
-  ;;
+  ;; "Elapsed time: 7.462084 msecs"
+  ;=> 1660292
   (time (part-1 input))
 
-  ;;
+  ;; "Elapsed time: 3.596292 msecs"
+  ;=> 22776016
   (time (part-2 input))
   )
