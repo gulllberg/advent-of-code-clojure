bdcb922dee6180606c1927f90b1b0582f7d165eb
Daniel Gullberg <daniel@gullberg.dev>
2025-05-11 10:12:37 -0400

    2015 day 6 - only keep necessary positions in state

--- src/advent_of_code/year_2015/day_06.clj
+++ src/advent_of_code/year_2015/day_06.clj
@@ -4,29 +4,23 @@
 (def input (slurp "src/advent_of_code/year_2015/inputs/day06.txt"))
 
 (defn update-lights
-  [state [x1 y1 x2 y2] update-fn]
+  [state [x1 y1 x2 y2] reduce-fn]
   (->> (for [x (range x1 (inc x2))
              y (range y1 (inc y2))]
          [x y])
-       (reduce (fn [state p]
-                 (update-fn state p))
-               state)))
-
-(defn create-state
-  []
-  (update-lights {} [0 0 999 999] (fn [state p] (assoc state p false))))
+       (reduce reduce-fn state)))
 
 (defn turn-on
   [state coords]
-  (update-lights state coords (fn [state p] (assoc state p true))))
+  (update-lights state coords (fn [state p] (conj state p)) ))
 
 (defn turn-off
   [state coords]
-  (update-lights state coords (fn [state p] (assoc state p false))))
+  (update-lights state coords (fn [state p] (disj state p))))
 
 (defn toggle
   [state coords]
-  (update-lights state coords (fn [state p] (update state p not))))
+  (update-lights state coords (fn [state p] (if (contains? state p) (disj state p) (conj state p)))))
 
 (defn do-instruction
   [state line]
@@ -39,26 +33,20 @@
 (defn part-1
   [input]
   (->> (clojure.string/split-lines input)
-       (reduce do-instruction (create-state))
-       (vals)
-       (filter true?)
+       (reduce do-instruction #{})
        (count)))
-
-(defn create-state-2
-  []
-  (update-lights {} [0 0 999 999] (fn [state p] (assoc state p 0))))
 
 (defn turn-on-2
   [state coords]
-  (update-lights state coords (fn [state p] (update state p inc))))
+  (update-lights state coords (fn [state p] (update state p (fn [v] (if v (inc v) 1))))))
 
 (defn turn-off-2
   [state coords]
-  (update-lights state coords (fn [state p] (update state p (fn [v] (if (zero? v) 0 (dec v)))))))
+  (update-lights state coords (fn [state p] (update state p (fn [v] (if (and v (> v 0)) (dec v) 0))))))
 
 (defn toggle-2
   [state coords]
-  (update-lights state coords (fn [state p] (update state p (fn [v] (+ v 2))))))
+  (update-lights state coords (fn [state p] (update state p (fn [v] (if v (+ v 2) 2))))))
 
 (defn do-instruction-2
   [state line]
@@ -71,16 +59,16 @@
 (defn part-2
   [input]
   (->> (clojure.string/split-lines input)
-       (reduce do-instruction-2 (create-state-2))
+       (reduce do-instruction-2 {})
        (vals)
        (reduce +)))
 
 (comment
   (time (part-1 input))
-  ;; "Elapsed time: 13620.854917 msecs"
+  ;; "Elapsed time: 9775.488667 msecs"
   ;=> 543903
 
   (time (part-2 input))
-  ;; "Elapsed time: 15178.803834 msecs"
+  ;; "Elapsed time: 13857.069625 msecs"
   ;=> 14687245
   )
