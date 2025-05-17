dac2d410efd5b990dad1c2c71c40b5c028b9a0be
Daniel Gullberg <daniel@gullberg.dev>
2025-05-17 19:27:28 -0400

    2015 day 25 with Cantor pairing function and iterate

--- src/advent_of_code/year_2015/day_25.clj
+++ src/advent_of_code/year_2015/day_25.clj
@@ -3,26 +3,18 @@
 
 (def input (slurp "src/advent_of_code/year_2015/inputs/day25.txt"))
 
-(defn get-first-number-on-row
+;; https://en.wikipedia.org/wiki/Pairing_function#Cantor_pairing_function
+(defn cantor-pairing-function
   {:test (fn []
-           (is= (get-first-number-on-row 1) 1)
-           (is= (get-first-number-on-row 3) 4)
-           (is= (get-first-number-on-row 6) 16))}
-  [row]
-  (->> (range)
-       (take row)
-       (reduce +)
-       (inc)))
-
-(defn get-difference-between-column-1-and-n
-  {:test (fn []
-           (is= (get-difference-between-column-1-and-n 1 3) 5)
-           (is= (get-difference-between-column-1-and-n 3 3) 9)
-           (is= (get-difference-between-column-1-and-n 3 2) 4)
-           (is= (get-difference-between-column-1-and-n 2 5) 18))}
-  [row column]
-  (->> (range (inc row) (+ row column))
-       (reduce +)))
+           (is= (cantor-pairing-function 0 0) 0)
+           (is= (cantor-pairing-function 1 0) 1)
+           (is= (cantor-pairing-function 0 1) 2)
+           (is= (cantor-pairing-function 1 1) 4)
+           (is= (cantor-pairing-function 2 2) 12)
+           (is= (cantor-pairing-function 3 1) 11)
+           (is= (cantor-pairing-function 2 3) 18))}
+  [x y]
+  (/ (+ (* x x) x (* 2 x y) (* 3 y) (* y y)) 2))
 
 (defn get-order-in-sequence
   {:test (fn []
@@ -30,9 +22,7 @@
            (is= (get-order-in-sequence 5 2) 17)
            (is= (get-order-in-sequence 2 4) 14))}
   [row column]
-  (let [first-number-on-row (get-first-number-on-row row)
-        difference-between-column-1-and-n (get-difference-between-column-1-and-n row column)]
-    (+ first-number-on-row difference-between-column-1-and-n)))
+  (inc (cantor-pairing-function (dec row) (dec column))))
 
 (defn get-next-code
   {:test (fn []
@@ -46,17 +36,12 @@
   {:test (fn []
            (is= (get-code-n 6) 17289845))}
   [n]
-  (loop [code 20151125
-         n n]
-    (if (= n 1)
-      code
-      (recur (get-next-code code) (dec n)))))
+  (nth (iterate get-next-code 20151125) (dec n)))
 
 (defn part-1
   [input]
   (let [[row column] (map read-string (re-seq #"\d+" input))]
     (get-code-n (get-order-in-sequence row column))))
-
 
 (comment
   (time (part-1 input))
